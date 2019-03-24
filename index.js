fe6599dc9ee5e8e0c65b5fc323f361275409dea4
A153865 <jfernandezcor@bankia.com>
2019-03-24 17:48:57 +0100

    +

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const LOCAL_PORT  = 3000
 
 const server = sv.server(LOCAL_PORT,
     [
-        {path: '/api', host:"iob.ms.epd.bankia.int", port: 41180}
+        {path: '/api', host:"iob.ms.epd.bankia.int", port: 41180},
+        {path: '/tabit', host:"tabit-epd.cm.es", port: 80, pathr: "/api/1.0/sap"},
     ]
 )
