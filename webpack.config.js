2570c5b2c272b44f6152b42dd33e8608798f75b7
A153865 <jfernandezcor@bankia.com>
2019-03-25 13:42:33 +0100

    +

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './lib/client/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+let config = {
+  entry: [path.resolve(__dirname,'./lib/client/index.js')],
+  output: {
+    path: path.resolve(__dirname,'./lib/build'),
+    filename: 'index_bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.css', '.scss'],
+    alias: {
+      //cmp: path.resolve(__dirname,'./client/componentes')
+    }
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.scss$/, 
+        use: [
+            {loader:'style-loader'},
+            {loader: 'css-loader', options: {modules: true, localIdentName: '[name]__[local]___[hash:base64:5]', sourceMap: true}},
+            {loader: 'sass-loader', options: {/*sourceMap: true*/}}
+          ]
+      },
+      { test: /\.css$/, 
+        use: [
+            {loader:'style-loader'},
+            {loader: 'css-loader', options: {modules: true, localIdentName: '[name]__[local]___[hash:base64:5]', sourceMap: true}}
+        ]
+      },
+      {
+        test: /\.(woff|png|gif|svg)$/,
+        loader: 'url-loader',
+      }
+    ]
+  },
+  plugins: [HtmlWebpackPluginConfig]
+}
+module.exports = config
